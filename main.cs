7d7a311ff5a6b5aa0b9d0efc8145c52186d12701
K-0de <ochampaughkaleb@outlook.com>
2024-05-27 02:52:46 +0000

    Created Unit Test 1 for Lucinda Potter PersonalInfo

--- main.cs
+++ main.cs
@@ -1,7 +1,18 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class PersonalInfo
+{
+    static void Main()
+    {
+
+        string name = "Lucinda Potter";
+        DateTime birthdate = new DateTime(1992, 6, 24);
+        string workPhone = "000-000-0101";
+        string cellPhone = "000-000-0189";
+           Console.WriteLine(name);
+           Console.WriteLine(birthdate.ToString("MM/dd/yyyy"));
+           Console.WriteLine("workphone " + workPhone);
+           Console.WriteLine("cellphone " + cellPhone);
+        
+        }
 }
