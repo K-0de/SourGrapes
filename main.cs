ee7dbd957d79fb6f27455fc256caf5fb869f0add
K-0de <ochampaughkaleb@outlook.com>
2024-06-21 17:18:08 +0000

    committ new
    .

--- main.cs
+++ main.cs
@@ -1,18 +1,15 @@
-using System;
+using System; 
 
-class PersonalInfo
-{
-    static void Main()
-    {
-
-        string name = "Lucinda Potter";
-        DateTime birthdate = new DateTime(1992, 6, 24);
-        string workPhone = "000-000-0101";
-        string cellPhone = "000-000-0189";
-           Console.WriteLine(name);
-           Console.WriteLine(birthdate.ToString("MM/dd/yyyy"));
-           Console.WriteLine("workphone " + workPhone);
-           Console.WriteLine("cellphone " + cellPhone);
-        
-        }
-}
+class Program {
+  public static void Main (string[] args) {
+     Console.WriteLine("Enter text here");
+    int charCount = Console.ReadLine().Length;
+    if((charCount > 140)){
+      Console.WriteLine("Rejected");
+      }
+    else Console.WriteLine("Posted");
+    
+  }
+  
+    }
+   
