0fab870c7a25983e5f10dc91f0c518fc160e2a2a
K-0de <ochampaughkaleb@outlook.com>
2024-07-12 21:33:26 +0000

    NEW Commit TemperaturesComparison 5.2

--- main.cs
+++ main.cs
@@ -4,43 +4,52 @@ class Program
 {
     static void Main()
     {
-        
-     
-      static bool IsPerfect(int number)
-      {
-          int sumOfDivisors = 0;
+         int[] temperatures = new int[5];
+         bool isWarmer = true;
+         bool isColder = true;
+         bool isValid = false;
+       Console.WriteLine("Enter 5 daily temperatures (between -30 and 130 degrees Farenheit):");
+        for (int i = 0; i < 5; i++){
+        while (!isValid){
+            if(int.TryParse(Console.ReadLine(), out temperatures[i])){
+            if(temperatures[i] >= -30 && temperatures[i] <= 130){
+                isValid = true;
+                
+                if(i > 0){
+                    if(temperatures[i] > temperatures[i - 1]){
+                        isColder = false;
+                        } else if (temperatures[i] < temperatures[i - 1]){
+                        isWarmer = false;
+                        }
+                        }
+                    
+                }
+                
+                
+            else{
+                    Console.WriteLine("Temperature " + temperatures[i] + " is invalid, Please enter a valid temperature between -30 and 130");
+                }   
+                
+                }
+            else {
+            Console.Write("Please enter a valid integer");
+            } 
 
-          for (int i = 1; i <= number / 2; i++)
-          {
-              if (number % i == 0)
-              {
-                  sumOfDivisors += i;
-              }
-          }
-
-          return sumOfDivisors == number;
-      }
-      
-      
-      
-      
-      Console.WriteLine("Enter a number between 1 and 10,000 to determine :");
-
-        string input = Console.ReadLine();
-
-        if (int.TryParse(input, out int number))
-        {
-            if (IsPerfect(number))
-            {
-                Console.WriteLine("Perfect");
-            }
-            else
-            {
-                Console.WriteLine("Not Perfect");
-            }
+        }  isValid = false;
         }
-       
+    if(isWarmer && !isColder){
+        Console.WriteLine("Getting warmer");
+    } else if(isColder && !isWarmer){
+        Console.WriteLine("Getting cooler");
+    } else {
+        Console.WriteLine("It's a mixed bag");
     }
+         Console.WriteLine("5-day temperature " + temperatures[0] + ","+ temperatures[1] +","+ temperatures[2] +","+ temperatures[3] +","+ temperatures[4]); 
+       double sumOfTemps= temperatures[0] + temperatures[1] + temperatures[2] + temperatures[3] + temperatures[4];
+        double averageTemp = sumOfTemps / 5;
+        Console.WriteLine("Average temperature is: " + averageTemp);
+        }
+}    
 
-   
-}
+
+
